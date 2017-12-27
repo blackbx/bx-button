87946b76ad262b0b6678bb5dbdd03462caf83ef3
Patrick Clover <patrick@blackbx.io>
2017-12-27 14:01:20 +0000

    builder

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const gulp         = require('gulp');
+const sourcemaps   = require('gulp-sourcemaps');
+const sass         = require('gulp-sass');
+const postcss      = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+gulp.task('styles', styles);
+gulp.task('scripts', scripts);
+gulp.task('default', ['styles', 'scripts']);
+
+function styles() {
+    return gulp
+        .src('src/*.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
+}
+
+function scripts() {
+    return gulp
+        .src('src/*.js')
+        .pipe(gulp.dest('dist'));
+}
+
