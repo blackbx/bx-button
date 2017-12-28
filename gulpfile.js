900eba41ae154792368106101f6723ea9ead009d
patrickclover <patrick@blackbx.io>
2017-12-28 15:32:14 +0000

    Hello

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
-const gulp         = require('gulp');
-const sourcemaps   = require('gulp-sourcemaps');
-const sass         = require('gulp-sass');
-const postcss      = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const concatCss    = require('gulp-concat-css');
+const gulp                 = require('gulp');
+const sourcemaps           = require('gulp-sourcemaps');
+const sass                 = require('gulp-sass');
+const postcss              = require('gulp-postcss');
+const autoprefixer         = require('autoprefixer');
+const concatCss            = require('gulp-concat-css');
+const angularTemplatecache = require('gulp-angular-templatecache');
+const del                  = require('del');
 
 gulp.task('styles', styles);
 gulp.task('scripts', scripts);
 gulp.task('html', html);
-gulp.task('default', ['styles', 'scripts', 'html']);
+gulp.task('clean', clean);
+gulp.task('default', ['clean', 'styles', 'scripts', 'html']);
 
 function styles() {
     return gulp
@@ -24,6 +27,9 @@ function styles() {
 function html() {
     return gulp
         .src('src/*.html')
+        .pipe(angularTemplatecache('html.js', {
+            module: 'bx.button'
+        }))
         .pipe(gulp.dest('dist'));
 }
 
@@ -31,5 +37,9 @@ function scripts() {
     return gulp
         .src('src/*.js')
         .pipe(gulp.dest('dist'));
+}
+
+function clean() {
+    return del(['dist']);
 }
 
